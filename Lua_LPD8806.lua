c06d0f13163f1f7dc8f66798610458017b753176
Doug McInnes <doug@dougmcinnes.com>
2015-03-28 15:09:30 -0700

    updated, now loading on ESP8266
    show not working yet

--- Lua_LPD8806.lua
+++ Lua_LPD8806.lua
@@ -1,15 +1,23 @@
-LPD8806 = {
-  leds = {}
-}
+local LPD8806 = {}
+LPD8806.__index = LPD8806
 
-function LPD8806:new (led_count, data_pin, clock_pin)
+function LPD8806.new(led_count, data_pin, clock_pin)
+  local self = setmetatable({}, LPD8806)
+  self.leds       = {}
   self.led_count  = led_count
   self.byte_count = led_count * 3
   self.data_pin   = data_pin
   self.clock_pin  = clock_pin
 
-  gpio.mode(data_pin,  gpio.OUTPUT)
-  gpio.mode(clock_pin, gpio.OUTPUT)
+  self:setup()
+
+  return self
+end
+
+function LPD8806.setup(self)
+  -- set pin modes
+  gpio.mode(self.data_pin,  gpio.OUTPUT)
+  gpio.mode(self.clock_pin, gpio.OUTPUT)
 
   for i=0, self.byte_count do
     -- highest most bit must be 1
@@ -17,18 +25,18 @@ function LPD8806:new (led_count, data_pin, clock_pin)
   end
 end
 
-function LPD8806:begin()
+function LPD8806.begin(self)
   -- send those zero bytes to clear the strip's data
   gpio.write(self.data_pin, gpio.LOW)
 
-  local count = math.floor(((byte_count+31)/32)*8)
+  local count = math.floor(((self.byte_count+31)/32)*8)
   for i=0, count do
     gpio.write(self.clock_pin, gpio.HIGH)
     gpio.write(self.clock_pin, gpio.LOW)
   end
 end
 
-function LPD8806:setPixelColor(num, r, g, b)
+function LPD8806.setPixelColor(self, num, r, g, b)
   -- strip color order is GBR for some strange reason
   -- ORing so higest most bit is still 1
   self.leds[num]   = bit.bor(g, 0x80)
@@ -36,10 +44,9 @@ function LPD8806:setPixelColor(num, r, g, b)
   self.leds[num+2] = bit.bor(r, 0x80)
 end
 
-function LPD8806:show()
+function LPD8806.show(self)
   -- iterate over led color value
   for i=0, self.byte_count do
-    i = i - 1
     local byte = self.leds[i]
 
     -- iterate over every bit
